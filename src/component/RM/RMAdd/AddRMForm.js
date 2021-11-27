a6f1cdd8f00d9c6565a90ed551f9b29da9019276
imsat16 <wepeg44@gmail.com>
2021-11-27 16:26:12 +0700

    Edit Reservation Management
    Edit Step 1 & 2 inside add modal component & add Material UI

--- src/component/RM/RMAdd/AddRMForm.js
+++ src/component/RM/RMAdd/AddRMForm.js
@@ -1,135 +1,193 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { multiStepContext } from "../StepContext";
 
 function AddRMForm() {
-  const{setStep, userData, setUserData} = useContext(multiStepContext);
+  const { setStep, userData, setUserData } = useContext(multiStepContext);
+  const [disabled, setDisabled] = useState(false);
+
+  function handleGameClick() {
+    setDisabled(!disabled);
+  }
   return (
     <div>
       <div className="flex-row">
-        <div className="lg:flex">
-          <div>
-            <h4 className="font-bold my-2">Common Information</h4>
-            <div className="px-2 pl-0">
-              <h5>Code</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Booking Time</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Status</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>User</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Source</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Guarantee</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-          </div>
-
-          <div className="lg:px-3">
-            <h4 className="font-bold my-2">Room Information</h4>
-            <div className="px-2 pl-0">
-              <h5>CheckIn</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Check Out</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Adult</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Child</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h5>Infant</h5>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
-            </div>
-            <div className="px-2 pl-0">
-              <h6>Room</h6>{" "}
-              <input
-                type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
-              />
+        <div className="md:flex">
+          <div className="bg-gray-300 m-2 shadow-md px-2 text-gray-700 rounded-lg">
+            <h4 className="font-bold pt-1">Common Information</h4>
+            <div className="py-3">
+              <div className="">
+                <h5>Code</h5>
+                <input
+                  type="number"
+                  value={userData["Rcode"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, Rcode: e.target.value })
+                  }
+                  disabled={disabled}
+                  onClick={handleGameClick}
+                  class="rounded-lg p-2 bg-gray-300 focus:shadow placeholder-gray-500 focus:outline-none"
+                  placeholder="123 321"
+                />
+              </div>
+              <div className="">
+                <h5>Booking Time</h5>{" "}
+                <input
+                  type="datetime-local"
+                  disabled={disabled}
+                  onClick={handleGameClick}
+                  value="2021-06-12T19:30"
+                  onChange={(e) =>
+                    setUserData({ ...userData, datetime: e.target.value })
+                  }
+                  name="booking-time"
+                  min="2018-06-07T00:00"
+                  max="2045-06-14T00:00"
+                  class="rounded-lg p-2 bg-gray-300 focus:shadow placeholder-gray-500 focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Status</h5>{" "}
+                <input
+                  type="text"
+                  value={userData["status"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, status: e.target.value })
+                  }
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>User</h5>{" "}
+                <input
+                  type="text"
+                  value={userData["user"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, user: e.target.value })
+                  }
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Source</h5>{" "}
+                <input
+                  type="text"
+                  value={userData["source"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, source: e.target.value })
+                  }
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Guarantee</h5>{" "}
+                <input
+                  type="text"
+                  value={userData["guarantee"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, guarantee: e.target.value })
+                  }
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
             </div>
           </div>
+          <div className="px-2 py-1"></div>
 
+          <div className=" bg-gray-300 m-2 shadow-md px-2 text-gray-700 rounded-lg">
+            <h4 className="font-bold pt-1">Room Information</h4>
+            <div className="py-3">
+              <div className="">
+                <h5>CheckIn</h5>{" "}
+                <input
+                  type="datetime-local"
+                  value={userData["checkin"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, checkin: e.target.value })
+                  }
+                  name="booking-time"
+                  min="2018-06-07T00:00"
+                  max="2045-06-14T00:00"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Check Out</h5>{" "}
+                <input
+                  type="datetime-local"
+                  value={userData["checkout"]}
+                  onChange={(e) =>
+                    setUserData({ ...userData, checkout: e.target.value })
+                  }
+                  name="booking-time"
+                  min="2018-06-07T00:00"
+                  max="2045-06-14T00:00"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Adult</h5>{" "}
+                <input
+                  type="number"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Child</h5>{" "}
+                <input
+                  type="number"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Infant</h5>{" "}
+                <input
+                  type="number"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+              <div className="">
+                <h5>Room</h5>{" "}
+                <input
+                  type="text"
+                  class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                  placeholder="Search anything..."
+                />
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className="pr-2">
-        <div className="float-right flex items-center rounded-lg mt-2 text-gray-600 ">
-          <button className="uppercase pl-2 flex font-semibold" onClick={()=>setStep(2)}>
-            Next{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+          <div className="float-right flex items-center rounded-lg mt-2 text-gray-600 ">
+            <button
+              className="uppercase pl-2 flex font-semibold"
+              onClick={() => setStep(2)}
             >
-              <path
-                fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
+              Next{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/component/RM/RMAdd/AddRMForm2.js
+++ src/component/RM/RMAdd/AddRMForm2.js
@@ -1,62 +1,133 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { multiStepContext } from "../StepContext";
 
 function AddRMForm2() {
   const { setStep, userData, setUserData } = useContext(multiStepContext);
+  const [disabled, setDisabled] = useState(false);
 
+  function handleGameClick() {
+    setDisabled(!disabled);
+  }
   return (
     <div>
       <div className="flex-row">
         <div className="lg:flex">
-          <div className="">
-            <h4 className="font-bold my-2">Guest Information</h4>
-            <div className="px-2 pl-0">
-              <h5>Guest Name</h5>{" "}
+          <div className=" bg-gray-300 m-2 shadow-md px-2 text-gray-700 rounded-lg">
+              <h4 className="font-bold pt-1">Guest Information</h4>
+            <div className="py-3">
+            <div className="">
+              <h5>Guest Name</h5>
               <input
                 type="text"
                 value={userData['guestname']}
+                disabled={disabled}
+                onClick={handleGameClick}
                 onChange={(e)=>setUserData({...userData, "guestname" : e.target.value})}
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                class="rounded-lg p-2 bg-gray-300 shadow focus:outline-none"
                 placeholder="Search anything..."
               />
             </div>
-            <div className="px-2 pl-0">
+            <div className="">
               <h5>Company</h5>{" "}
               <input
                 type="text"
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
-                placeholder="Search anything..."
+                value={userData['company']}
+                disabled={disabled}
+                onClick={handleGameClick}
+                onChange={(e)=>setUserData({...userData, "company" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-300 shadow focus:outline-none"
+                placeholder="Company..."
               />
             </div>
-            <div className="px-2 pl-0">
+            <div className="">
               <h5>Email</h5>{" "}
               <input
                 type="email"
                 value={userData['email']}
                 onChange={(e)=>setUserData({...userData, "email" : e.target.value})}
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
                 placeholder="example@gmail.com"
               />
             </div>
-            <div className="px-2 pl-0">
+            <div className="">
               <h5>Phone</h5>{" "}
               <input
                 type="tel"
                 value={userData['phone']}
                 onChange={(e)=>setUserData({...userData, "phone" : e.target.value})}
-                class="rounded-lg p-2 bg-gray-200 focus:shadow focus:outline-none"
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
                 placeholder="+1 234 567 890"
               />
             </div>
-            <div className="px-2 pl-0">
+            <div className="">
               <h5>Sepecial Requirement</h5>{" "}
               <textarea
                 role="form"
                 value={userData['sepecialrequirement']}
                 onChange={(e)=>setUserData({...userData, "sepecialrequirement" : e.target.value})}
-                class="rounded-lg w-full p-2 bg-gray-200 focus:shadow focus:outline-none"
+                class="rounded-lg w-full p-2 bg-gray-100 focus:shadow focus:outline-none"
                 placeholder="Search anything..."
               />
+            </div>
+            </div>
+          </div>
+
+          <div className="px-2 py-1"></div>
+          
+          <div className="bg-gray-300 m-2 shadow-md px-2 text-gray-700 rounded-lg">
+            <h4 className="font-bold pt-1">Billing Address</h4>
+            <div className="py-3">
+              <div className="">
+              <h5>Address</h5>{" "}
+              <input
+                type="text"
+                value={userData['address']}
+                onChange={(e)=>setUserData({...userData, "address" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
+                placeholder="Search anything..."
+              />
+            </div>
+            <div className="">
+              <h5>City</h5>{" "}
+              <input
+                type="text"
+                value={userData['city']}
+                onChange={(e)=>setUserData({...userData, "city" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
+                placeholder="Search anything..."
+              />
+            </div>
+            <div className="">
+              <h5>State</h5>{" "}
+              <input
+                type="text"
+                value={userData['state']}
+                onChange={(e)=>setUserData({...userData, "state" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div className="">
+              <h5>Country</h5>{" "}
+              <input
+                type="text"
+                value={userData['country']}
+                onChange={(e)=>setUserData({...userData, "country" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
+                placeholder="lorem ipsum dolor"
+              />
+            </div>
+            <div className="">
+              <h5>Post Code</h5>{" "}
+              <input
+                type="number"
+                value={userData['postcode']}
+                onChange={(e)=>setUserData({...userData, "postcode" : e.target.value})}
+                class="rounded-lg p-2 bg-gray-100 focus:shadow focus:outline-none"
+                placeholder="Search anything..."
+              />
+            </div>
+            
             </div>
           </div>
         </div>
@@ -64,7 +135,7 @@ function AddRMForm2() {
           <div className="justify-between mt-2 flex items-center rounded-lg text-gray-600 ">
             <button
               onClick={() => setStep(1)}
-              className="uppercase pl-2 flex font-semibold"
+              className="uppercase flex font-semibold"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -82,9 +153,9 @@ function AddRMForm2() {
             </button>
             <button
               onClick={() => setStep(3)}
-              className="uppercase pl-2 flex font-semibold"
+              className="uppercase flex font-semibold"
             >
-              Next{" "}
+              Next
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
